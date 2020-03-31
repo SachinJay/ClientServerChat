134b240f8a19db3cbb3ea508b72cedec81415754
Sachin Devasahayam <sjd232@cornell.edu>
2020-03-31 12:49:05 -0400

    Copy-pasted code from last client/server project
    -Commented out all code except for the code which initializes the socket
    -after doing so, cleaned up code using try-catch

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,6 +1,50 @@
 package server;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import shared.Constants;
+
 public class Server
 {
+	public static void main(String[] args) 
+	{
+		//socket that the server connects to, same as the socket
+		//the client connects to
+		ServerSocket serverSocket = null;
+		try
+		{
+			serverSocket = new ServerSocket(Constants.PORT);
+		} catch (IOException e)
+		{
+			System.out.println(Constants.SERV_ERR_MSG);
+			e.printStackTrace();
+		}
+		
+		//When Client.java is run, it will connect to the server 
+		//socket above, this below line waits and listens for this event
+		//to occur
+		//Socket socket = serverSocket.accept();
+		
+//		//Will only print when the client connects
+//		System.out.println("Client Connected");
+//		
+//		//Input stream comes from the accepted socket
+//		InputStreamReader isr = new InputStreamReader(socket.getInputStream());
+//		
+//		BufferedReader reader = new BufferedReader(isr);
+//		String message = reader.readLine();
+//		System.out.println("client said: " + message);
+//		
+//		PrintWriter pr = new PrintWriter(socket.getOutputStream());
+//		pr.println("This is a test message from the server");
+//		pr.flush();
+	}
+
 
 }
