4b67ce87382dfed2f25dcd9c94b761394c25e499
Sachin Devasahayam <sjd232@cornell.edu>
2020-03-31 12:48:09 -0400

    Created Constants class

---
+++ src/shared/Constants.java
@@ -0,0 +1,8 @@
+package shared;
+
+public class Constants
+{
+	public static final int PORT = 4999;
+	public static final String SERV_ERR_MSG = "Something went wrong, could not listen on port " + PORT;
+
+}
