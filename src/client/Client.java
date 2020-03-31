cbf99d0493def40dbcbcaa32387f7f6650f5da30
Sachin Devasahayam <sjd232@cornell.edu>
2020-03-31 12:56:08 -0400

    Copy-pasted client code from previous project
    -Commented out all code that wasn't initial setup
    -Refactored code to use try-catch
    -Put in error messages

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,6 +1,43 @@
 package client;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import shared.Constants;
+
 public class Client
 {
-
+	public static void main(String[] args)
+	{
+		//Socket that client connects to
+		Socket socket = null;
+		try
+		{
+			socket = new Socket(Constants.HOST_NAME, Constants.PORT);
+		}
+		catch (UnknownHostException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		catch (IOException e)
+		{
+			System.out.println(Constants.SERV_ERR_MSG);
+			e.printStackTrace();
+		}
+		
+//		PrintWriter pr = new PrintWriter(socket.getOutputStream());
+//		pr.println("This a test message from the client");
+//		pr.flush();
+//		
+//		InputStreamReader isr = new InputStreamReader(socket.getInputStream());
+//		
+//		BufferedReader reader = new BufferedReader(isr);
+//		String message = reader.readLine();
+//		System.out.println("Server said: " + message);
+	}
 }
--- src/shared/Constants.java
+++ src/shared/Constants.java
@@ -3,6 +3,9 @@ package shared;
 public class Constants
 {
 	public static final int PORT = 4999;
+	public static final String HOST_NAME = "localhost";
+	
 	public static final String SERV_ERR_MSG = "Something went wrong, could not listen on port " + PORT;
-
+	public static final String CLI_IO_ERR_MSG = "Something went wrong, could not connect to port" + PORT;
+	public static final String UNKNOWN_HOST_ERR_MSG = "Something went wrong, unknown host: " + HOST_NAME;
 }
