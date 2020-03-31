3a6e90711ef400586709d6bc48ab70e94ef61bb3
Sachin Devasahayam <sjd232@cornell.edu>
2020-03-31 13:24:21 -0400

    Implemented serverThread
    -Did so in exact same way as client thread

---
+++ src/client/ServerThread.java
@@ -0,0 +1,39 @@
+package client;
+
+import java.net.Socket;
+
+public class ServerThread implements Runnable
+{
+	/**
+	 * The socket returned when server accepts a client
+	 */
+	private Socket socket; 
+	
+	public ServerThread(Socket socket)
+	{
+		this.setSocket(socket);
+	}
+
+	@Override
+	public void run()
+	{
+		// TODO Implement chat
+		
+	}
+
+	/**
+	 * @return the socket
+	 */
+	public Socket getSocket()
+	{
+		return socket;
+	}
+
+	/**
+	 * @param socket the socket to set
+	 */
+	public void setSocket(Socket socket)
+	{
+		this.socket = socket;
+	}
+}
